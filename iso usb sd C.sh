84c6faab491274eb54f876a08143c4362bc39828
LostByteSoft <mad_mike_41@hotmail.com>
2019-12-07 09:22:29 -0500

    Add files via upload

---
+++ iso usb sd C.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+echo "Select filename using dialog"
+FILE="$(zenity --file-selection --title="Select a File")"
+echo "Your file is $FILE"
+sleep 1
+
+# -----------------------------------------------------------------------------
+
+if test -z "$FILE"
+then
+      echo "\$FILE is empty and now exit"
+      sleep 1
+      read -n 1 -s -r -p "Press any key to continue"
+      exit
+else
+      echo "\$FILE is NOT empty it contain $FILE"
+fi
+sleep 1
+
+# -----------------------------------------------------------------------------
+
+VAR="$FILE"
+echo "${VAR}"
+sleep 1
+
+sudo dd if="$FILE" of=/dev/sdc
