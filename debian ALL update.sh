04e322cd43dc00695bcca6422e178192ea2398f1
LostByteSoft <mad_mike_41@hotmail.com>
2021-02-15 15:05:27 -0500

    Add files via upload

--- debian ALL update.sh
+++ debian ALL update.sh
@@ -1,13 +1,12 @@
 echo -------------------------
 echo "LostBytesSoft"
 echo "Auto updater for Debian 10"
-echo "Version 2020-02-26"
+echo "Version 2021-01-18"
+echo -------------------------
 sudo apt update -y
+echo -------------------------
 sudo apt upgrade -y
 echo -------------------------
-sleep 2
-echo "LostBytesSoft"
-echo "Version 2020-02-26"
 echo "Autoremove no longer used files"
 sudo apt-get autoremove -y
 echo -------------------------
