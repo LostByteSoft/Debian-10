8ed1fab44b5968979cb43503bafb15d6901f6ef8
LostByteSoft <mad_mike_41@hotmail.com>
2021-12-15 09:36:23 -0500

    Delete WIPE sdd.sh

--- WIPE sdd.sh
+++
@@ -1,7 +0,0 @@
-echo "SDC, wipe mbr"
-sudo dd if=/dev/zero of=/dev/sdd bs=446 count=1 #*replace X with the target drive letter.
-sleep 1
-echo "wipe part with 0"
-sudo dd if=/dev/zero of=/dev/sdd bs=1M
-sleep 30
-exit
