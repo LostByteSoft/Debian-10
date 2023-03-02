7c86d47d0bf563af0c4828336af91c8957655ad9
LostByteSoft <mad_mike_41@hotmail.com>
2023-03-01 22:05:41 -0500

    Delete remove accessoires junk.sh

--- remove accessoires junk.sh
+++
@@ -1,7 +0,0 @@
-echo "Remove junk accessoires"
-sudo apt-get remove goldendict -y &&
-sudo apt-get remove gnote -y &&
-sudo apt-get remove yelp -y &&
-sudo apt-get remove khmerconverter -y &&
-sudo apt-get remove debian-reference-common -y &&
-exit
