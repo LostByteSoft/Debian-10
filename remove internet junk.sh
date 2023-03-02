236153eca166113a15b77f9fe17ad003f6a5b7d6
LostByteSoft <mad_mike_41@hotmail.com>
2023-03-01 22:05:17 -0500

    Delete remove internet junk.sh

--- remove internet junk.sh
+++
@@ -1,7 +0,0 @@
-echo "Remove junk internet"
-sudo apt-get remove pidgin -y &&
-sudo apt-get remove thunderbird -y &&
-sudo apt-get remove hexchat -y &&
-sudo apt-get remove transmission-gtk -y &&
-sudo apt-get remove vinagre -y &&
-exit
