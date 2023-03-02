24743be5fd3cc4f843cc7e4e1e38a61104772d02
LostByteSoft <mad_mike_41@hotmail.com>
2023-03-01 22:05:08 -0500

    Delete remove audio junk.sh

--- remove audio junk.sh
+++
@@ -1,7 +0,0 @@
-echo "Remove junk audio"
-sudo apt remove totem -y &&
-sudo apt remove brasero -y &&
-sudo apt remove cheese -y &&
-sudo apt remove sound-juicer -y &&
-sudo apt remove gnome-sound-recorder -y &&
-exit
