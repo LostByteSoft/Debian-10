b7f3a2b97b2a939253cce19b0bf169e9e90183a4
LostByteSoft <mad_mike_41@hotmail.com>
2023-03-13 19:30:29 -0400

    Delete remove zzz rhythmbox.sh

--- remove zzz rhythmbox.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "https://techpiezo.com/linux/remove-a-package-without-removing-its-dependencies-in-ubuntu/"
-sudo dpkg -r --force-depends rhythmbox-plugins
-sudo dpkg -r --force-depends rhythmbox
-
-	echo Press ENTER key to exit !
-	read name
-	exit
